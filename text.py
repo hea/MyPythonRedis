f6c59b3da42a6e82fc494a7d9f4c955bbefb2030
luosm <luosaiming@yahoo.cn>
2013-03-18 18:21:14 +0800

    use config dbindex

--- text.py
+++ text.py
@@ -1,22 +1,27 @@
 __author__ = 'luosm'
 
+import config
 import time
 import datetime
 import redis
+import randomTest
 
 
-pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=0)
+pool = redis.ConnectionPool(host=config.REDIS_IP, port=config.REDIS_PORT, db=config.REDIS_DBINDEX)
 r = redis.Redis(connection_pool=pool)
 
 if __name__ == '__main__':
     # simple key test
     r.set('key', 'value')
     #print ('key value is %s', r.get('key'))
+    #print some random
+    randomTest.setRedisRandom(r)
 
-
+    print r.get('key2')
     # create pipeline##
     pipe = r.pipeline()
     pipe.set('pipeTest', 'pipeValue')
     print pipe.get('pipeTest')
     print pipe.execute()
     #print ('current time is %i', datetime.datetime.now())
+
